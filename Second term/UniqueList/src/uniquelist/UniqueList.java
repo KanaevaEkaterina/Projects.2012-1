518ae7fb21b9e5a5663523824e437401abc1501a
Kanaeva Yekaterina <kanaeva.katerina6@gmail.com>
2012-04-22 17:03:27 +0400

    addition of super-methods for uniqueList

--- Second term/UniqueList/src/uniquelist/UniqueList.java
+++ Second term/UniqueList/src/uniquelist/UniqueList.java
@@ -47,15 +47,7 @@ public class UniqueList extends List {
     @Override
     public void addToBegin(int value) throws RepeatingElement {
         if (searchForUniqueness(value)) {
-            ListElement newEl = new ListElement(value);
-            if (!isEmpty()) {
-                newEl.next = head;
-                head = newEl;
-            } else {
-                head = newEl;
-                tail = newEl;
-                newEl.next = null;
-            }
+            super.addToBegin(value);
         } else {
             throw new RepeatingElement();
         }
@@ -70,16 +62,7 @@ public class UniqueList extends List {
     @Override
     public void addToEnd(int value) throws RepeatingElement {
         if (searchForUniqueness(value)) {
-            ListElement newEl = new ListElement(value);
-            if (isEmpty()) {
-                head = newEl;
-                tail = newEl;
-                newEl.next = null;
-            } else {
-                newEl.next = null;
-                tail.next = newEl;
-                tail = newEl;
-            }
+           super.addToBegin(value);
         } else {
             throw new RepeatingElement();
         }
@@ -93,21 +76,7 @@ public class UniqueList extends List {
     @Override
     public void deleteElement(int value) throws ElementNotFound {
         if (!searchForUniqueness(value)) {
-            ListElement index = head;
-            if (index.value == value) {
-                deleteFromBegin();
-            } else {
-                while (index.next != null) {
-                    if (index.next.value == value) {
-                        index.next = index.next.next;
-                    } else {
-                        index = index.next;
-                    }
-                }
-                if ((index.next == null) && (index.value == value)) {
-                    deleteFromEnd();
-                }
-            }
+            super.deleteElement(value);
         } else {
             throw new ElementNotFound();
         }
@@ -121,24 +90,7 @@ public class UniqueList extends List {
     @Override
     public void add(int value) throws RepeatingElement {
         if (searchForUniqueness(value)) {
-            ListElement index = head;
-            ListElement further = new ListElement(value);
-            if (index == null || index.value >= value) {
-                addToBegin(value);
-            } else {
-                while (index.next != null) {
-                    if (index.next.value <= value) {
-                        index = index.next;
-                    } else {
-                        further.next = index.next;
-                        index.next = further;
-                        break;
-                    }
-                }
-                if (index.next == null) {
-                    addToEnd(value);
-                }
-            }
+            super.add(value);
         } else {
             throw new RepeatingElement();
         }
@@ -152,30 +104,10 @@ public class UniqueList extends List {
      */
     @Override
     public void addToPlace(int value, int number) throws RepeatingElement {
-        ListElement index = head;
-        if (number == 1 || index == null) {
-            addToBegin(value);
-        }
-        if ((number > 1) && (number <= numberOfElements())) {
-            while (index.next != null) {
-                for (int i = 1; i < number - 1; i++) {
-                    index = index.next;
-                }
-                if (searchForUniqueness(value)) {
-                    ListElement further = new ListElement(value);
-                    further.next = index.next;
-                    index.next = further;
-                } else {
-                    throw new RepeatingElement();
-                }
-                break;
-            }
-            if (index.next == null) {
-                addToEnd(value);
-            }
-        }
-        if (number == numberOfElements() + 1) {
-            addToEnd(value);
+        if (searchForUniqueness(value)) {
+            super.addToPlace(value, number);
+        } else {
+            throw new RepeatingElement();
         }
     }
 }
