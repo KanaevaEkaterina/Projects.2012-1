bfce0eba82aa72ef9173bf194a1bfbabae79646d
Kanaeva Yekaterina <kanaeva.katerina6@gmail.com>
2013-02-16 23:05:51 +0400

    add some files

---
+++ Fourth term/1.1/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+ 
+let rec factorial n =
+    if n < 1 then 1
+    else n * factorial (n - 1)
+     
+Console.WriteLine("Write number")
+let x : int = int (Console.Read()) - 48
+Console.ReadLine()
+Console.Write(factorial x)
+Console.Read()
+
