6999f9af88903c23e18571354b244fe794d21efb
Kanaeva Yekaterina <kanaeva.katerina6@gmail.com>
2013-02-16 23:07:53 +0400

    add some files

---
+++ Fourth term/1.2/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec fibNum f =
+    if f <= 2 then 1
+    else fibNum (f - 1) + fibNum (f - 2)
+
+Console.WriteLine("Write number")
+let x : int = int (Console.Read()) - 48
+Console.ReadLine()
+Console.Write(fibNum x)
+Console.Read()
+
