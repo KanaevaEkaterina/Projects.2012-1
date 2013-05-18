c4a98ca5c5c91a910efc4d117197182be67d6313
KanaevaEkaterina <kanaeva.katerina6@gmail.com>
2013-05-19 00:52:53 +0400

    add 3.1 and 3.4

---
+++ Fourth term/3.1/maxSumInList/maxSumInList/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+let rec sumOfElementsInList (list: List<int>) sum index : int = 
+    match list with
+    | lst when lst.Length <= 2 -> index
+    | head::second::tail -> 
+                            let newSum =   if (head + tail.Head > sum) 
+                                            then
+                                                head + tail.Head
+                                            else 
+                                                sum
+                            let index = if (head + tail.Head > sum)
+                                            then
+                                                if (index = 0)
+                                                    then
+                                                        index + 2
+                                                    else
+                                                        index + 1
+                                            else
+                                                index
+                            sumOfElementsInList list.Tail newSum index 
+
+let printList (a : list<int>)  = 
+    List.iter (fun x -> printf "%A " x) a
+
+let list = [2; 5; 3; 6; 1]
+
+let maxIndex = sumOfElementsInList list 0 0
+
+Console.Write("Element with maximum sum of neighbours in list ")
+printList(list)
+Console.Write(" is on position ")
+Console.WriteLine(maxIndex)
+Console.Read() |> ignore
---
+++ Fourth term/3.4/heightOfTree/heightOfTree/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+type Tree = 
+    | Tree of int * Tree * Tree
+    | Node of int option
+
+let rec treeHeight (tree:Tree) height : int = 
+    match tree with
+    | Node(_) -> height
+    | Tree(_, left, right) ->   if (treeHeight left height) >= (treeHeight right height)
+                                    then
+                                        treeHeight left height + height
+                                    else
+                                        treeHeight right height + height
+
+let tree = Tree(5, Tree(4, Node(Some(3)), Node(None)), Tree(7, Node(Some(6)), Tree(9, Node(Some(8)), Tree(11, Node(None), Node(Some(12))))))
+let height = treeHeight tree 1
+Console.Write(height)
+Console.Read() |> ignore
