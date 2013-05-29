69c32b4021a8b3b7f0c8edbe4fbf54514d1c6f14
KanaevaEkaterina <kanaeva.katerina6@gmail.com>
2013-05-29 12:16:27 +0400

    correct threads

--- Fourth term/7.1/Threads/Threads/Program.fs
+++ Fourth term/7.1/Threads/Threads/Program.fs
@@ -4,6 +4,7 @@ let mutable result = 0
 let array = Array.init 1000000 (fun x -> 1)
 let isWorkerFinished = Array.init 100 (fun x -> false)
 
+Console.Write("First: ")
 for i = 0 to 99 do      
     let worker = new System.ComponentModel.BackgroundWorker()
     worker.DoWork.Add(fun args -> for j = 0 to 9999 do result <- result + array.[i * 10000 + j])
@@ -11,6 +12,16 @@ for i = 0 to 99 do
     worker.RunWorkerAsync()
 
 while not (Array.forall (fun b -> b = true) isWorkerFinished) do ()
-    
-Console.Write(result)
+Console.WriteLine(result)
+
+Console.Write("Second: ")
+for i = 0 to 99 do      
+    let worker = new System.ComponentModel.BackgroundWorker()
+    worker.DoWork.Add(fun args -> for j = 0 to 9999 do result <- result + array.[i * 10000 + j])
+    worker.RunWorkerCompleted.Add(fun args -> isWorkerFinished.[i] <- true)
+    worker.RunWorkerAsync()
+
+while not (Array.forall (fun b -> b = true) isWorkerFinished) do ()
+Console.WriteLine(result)
+
 Console.ReadLine() |> ignore
