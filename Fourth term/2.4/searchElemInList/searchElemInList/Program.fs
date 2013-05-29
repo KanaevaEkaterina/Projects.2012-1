882cb255d682311376346ebe36dba98edd5b913b
KanaevaEkaterina <kanaeva.katerina6@gmail.com>
2013-05-29 11:55:15 +0400

    add new variant for search of element in list

--- Fourth term/2.4/searchElemInList/searchElemInList/Program.fs
+++ Fourth term/2.4/searchElemInList/searchElemInList/Program.fs
@@ -1,13 +1,11 @@
 ﻿open System
 open Microsoft.FSharp.Core.Operators
 
-let searchElem (l:list<int>, elem:int): int = 
-    let mutable flag = true
-    let mutable index = 0;
-    while flag && (index < l.Length) do
-        if l.Item(index) = elem then flag <- false
-        else index <- index + 1
-    index + 1
+let rec searchElem (l:list<int>) elem pos bool : int = 
+    match l with
+    | head::tail -> if bool then
+                       pos
+                    else searchElem tail elem pos+1 bool 
 
 let printList (a : list<int>)  = 
     List.iter (fun x -> printf "%A " x) a
@@ -19,8 +17,8 @@ printList list1
 Console.WriteLine()
 Console.Write("Write element for searching: ")
 let element = int(Console.ReadLine()) - 48
-let result = searchElem (list1, element)
+let result = searchElem list1 element 0 false
 Console.Write("Position of this element is ");
-Console.Write(result);
-Console.Read()
+Console.Write(result)
+Console.Read() |> ignore
 
---
+++ Fourth term/2.4_0/searchElementInList/searchElementInList/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+let rec searchElem (l:list<int>) elem pos (flag: bool) : int = 
+    match l with
+    | head::tail -> if flag then
+                       pos
+                    else 
+                        let flag = 
+                                    if (head = elem) then
+                                        true
+                                    else
+                                        false
+                        let pos = pos + 1
+                        searchElem tail elem pos flag 
+    | [] -> if flag then 
+                pos
+            else
+                0
+        
+
+let printList (a : list<int>)  = 
+    List.iter (fun x -> printf "%A " x) a
+
+let list1 = [1; 4; 2; 5; 7]
+
+Console.Write("List: ")
+printList list1
+Console.WriteLine()
+Console.Write("Write element for searching: ")
+let element = Console.Read()-48
+let result = searchElem list1 element 0 false
+Console.Write("Position of this element is ");
+Console.WriteLine(result)
+Console.ReadLine() |> ignore
